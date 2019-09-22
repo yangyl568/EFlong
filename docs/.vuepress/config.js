5f1cc4a40b8b57ba71007efa0b8bace4d90b4743
yangyl568 <273735803@qq.com>
2019-09-22 16:55:45 +0800

    '创建目录'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,16 +3,17 @@ module.exports = {
     description: 'Just playing around',
     themeConfig: {
         nav: [
-            { text: '接口定义', link: '/apiword' },
-            { text: '接口字段定义', link: '/api' },
-            { text: '附录：错误码', link: '/error' }
+            { text: '知识点', link: '/' },
+            { text: '面试~', link: '/mianshi' },
+            { text: '扩展', link: '/kuozhan' },
+            { text: '报错信息', link: '/error' }
         ],
         sidebar: {
             '/': [
                 "/", //指的是根目录的md文件 也就是 README.md 里面的内容
-                // "apiword", 根目录创建 apiword.md文件
-                // "api", 根目录创建 api.md文件
-                // "error" 根目录创建 error.md文件
+                "mianshi", //根目录创建 mianshi.md文件
+                "kuozhan", //根目录创建 kuozhan.md文件
+                "error" //根目录创建 error.md文件
             ]
         },
         sidebarDepth: 2
