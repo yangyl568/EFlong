5e18605542966a760a85f6b8ce984cc22992c865
yangyl568 <273735803@qq.com>
2021-03-23 15:19:21 +0800

    feat: 修改 deploy 配置

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:yangyl568/EFlong.github.io.git master
+git push -f git@github.com:yangyl568/EFlong.github.io.git master:gh-pages
 
 cd -
